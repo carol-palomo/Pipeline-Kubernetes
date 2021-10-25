170513c3bfa9e192ae3865ec535334ec0e27deb6
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-25 16:16:02 -0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,3 +2,5 @@ cd /var/lib/jenkins/workspace/pipeline-K8s/ansible
 
 ANSIBLE_OUT=$(ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa)
 
+echo $ANSIBLE_OUT
+
