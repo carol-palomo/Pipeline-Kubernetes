e3610dc9612d9ba809700a0cd1ee106f57ce6a34
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-25 15:13:05 -0300

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,84 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ami_k8s" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.large" 
+  key_name                    = "id_rsa_jenkins" # key chave publica cadastrada na AWS 
+  vpc_security_group_ids      = ["${aws_security_group.acessos_k8s_jenkins.id}"]
+  subnet_id                   = "subnet-0ab487dbac2dcfa24"
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "k8s_jenkins-GamaOne"
+  }
+}
+
+resource "aws_security_group" "acessos_k8s_jenkins" {
+  name        = "acessos_k8s_jenkins"
+  description = "acessos inbound traffic"
+  vpc_id      = "vpc-0304dcb48c5e67fa0"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
+    },
+    {
+      description      = "Acesso HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"],
+      prefix_list_ids  = null,
+      security_groups : null,
+      self : null,
+      description : "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# terraform refresh para mostrar o ssh
+output "aws_instance_e_ssh" {
+  value = aws_instance.ami_k8s.public_dns
+
+}
+
+output "aws_instance_id" {
+  value = aws_instance.ami_k8s.id
+
+}
