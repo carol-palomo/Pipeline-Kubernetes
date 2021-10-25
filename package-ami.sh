b27fbc0439406df12b129bf3eaa1fe99100a0420
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-25 17:25:58 -0300

    Update package-ami.sh

---
+++ package-ami.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+cd /var/lib/jenkins/workspace/pipeline-K8s/terraform
+RESOURCE_ID=$(terraform output aws_instance_id | cut -b 2- | rev | cut -b 2- | rev )
+
+cd /var/lib/jenkins/workspace/pipeline-K8s/terraform-ami
+terraform init
+TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID terraform apply -auto-approve
